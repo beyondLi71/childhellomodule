f80a8cae3782616c58491b4736e5feaad5c3569f
beyondLi71 <liboyang71@163.com>
2018-03-12 10:18:18 +0800

    Update ChildhellomoduleApplication.java

--- src/main/java/com/beyondli/ChildhellomoduleApplication.java
+++ src/main/java/com/beyondli/ChildhellomoduleApplication.java
@@ -2,7 +2,7 @@ package com.beyondli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//测试
 @SpringBootApplication
 public class ChildhellomoduleApplication {
 
